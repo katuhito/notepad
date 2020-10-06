8a6fc4b96cad46731413880c32a8171e51eb5aeb
Katuhito Hara <katuhitohara@gmail.com>
2020-10-06 10:16:53 +0900

    Vue_CLI_MEMO

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        memos: [
+            { id: 1, body: 'サンプルのメモです' },
+        ]
+    },
+    mutations: {
+        save(state, memo) {
+            var max = state.memos[state.mamos.length - 1].id
+            memo.id = max + 1
+            state.memos.push(memo)
+        }
+    }
+})
